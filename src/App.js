9fc16f0bf2b165a7c2037575210e43b8794ce537
Ranjitha-cr? <ranjithacr@gmail.com>
2021-03-28 12:10:13 +0530

    currency-converter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CurrencyConverter from './components/CurrencyConverter'
+import './App.css'
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <CurrencyConverter/>
+    )  
+  }  
+}
+
+export default App;
